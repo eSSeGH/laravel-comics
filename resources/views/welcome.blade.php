71622fa3e340481f6ef9db7ef464209834b07072
eSSeGH <ste.virgadaula@gmail.com>
2023-04-11 11:43:49 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('main-content')
+<main>
+
+<div class="black-section">
+
+    <div class="container">
+
+        <div class="row">
+
+            @foreach($comics as $comic)
+            <div class="col">
+
+                <div class="card">
+                    <img src="{{ Vite::asset('$comic[thumb]') }}" alt="">
+                    <span>{{ $comic['title'] }}</span>
+                </div>
+
+            </div>
+            @endforeach
+
+        </div>
+
+    </div>
+
+</div>
+
+</main>
+@endsection
+
+@section('page-title', $page_title)
